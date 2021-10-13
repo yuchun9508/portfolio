29c62fa99dc77afdc8fda0469f5ad006ed198292
Chun <yuchun9508@gmail.com>
2021-10-13 13:19:25 +0800

    add custom tailwind config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
+const colors = {
+  jetBlack: '#000401',
+  gunmetalGray: '#4e545c',
+  gunmetalGrayDark: '#171C1c',
+  pewter: '#8d9797',
+  white: '#e5e8e8',
+}
+
+const fonts = {
+  inter: ['"Inter"', 'sans-serif'],
+}
+
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: colors.jetBlack,
+      secondary: {
+        DEFAULT: colors.gunmetalGray,
+        dark: colors.gunmetalGrayDark,
+      },
+      accent: colors.pewter,
+      neutral: colors.white,
+    },
+    extend: {
+      fontFamily: {
+        primary: fonts.inter,
+      }
+    },
   },
   variants: {
     extend: {},
