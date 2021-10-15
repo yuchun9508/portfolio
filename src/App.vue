37aad487ad9911e52eed4eb11b1cb70d8c135a38
Chun <yuchun9508@gmail.com>
2021-10-15 20:42:36 +0800

    add the-social-media-bar component

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <app-header></app-header>
     <router-view :class="{ 'filter blur-sm brightness-75': $store.state.isMenuOpened }" />
     <app-footer></app-footer>
+    <the-social-media-bar></the-social-media-bar>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import TheSocialMediaBar from '@/components/TheSocialMediaBar.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppFooter
+    AppFooter,
+    TheSocialMediaBar
   },
   watch: {
     // prevent background scrolling
---
+++ src/components/TheSocialMediaBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-12 fixed bottom-28 left-5 hidden lg:block sm:left-8 xl:left-20">
+        <ul class="-my-4 text-center">
+            <li class="inline-block my-4">
+                <a href="" target="_blank" rel="noopener noreferrer" class="inline-block">
+                    <span class="icon icon-linkedin w-10 h-10"></span>
+                </a>
+            </li>
+            <li class="inline-block my-4">
+                <a href="" target="_blank" rel="noopener noreferrer" class="inline-block">
+                    <span class="icon icon-github w-10 h-10"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheSocialMediaBar'
+}
+</script>
+
+<style lang="postcss" scoped>
+.icon-linkedin {
+    background-image: url('../assets/images/icon-linkedin.svg');
+}
+.icon-github {
+    background-image: url('../assets/images/icon-github.svg');
+}
+</style>
