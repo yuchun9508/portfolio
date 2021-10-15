bd325e1bfa6b464134afed8b915131c6c7555c3a
Chun <yuchun9508@gmail.com>
2021-10-15 20:42:17 +0800

    update app-footer component: add social media icon

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,34 @@
 <template>
   <footer class="py-10">
-    <div class="container-fluid">
-      <p class="text-center">© 2021 Yu-Chun Wu. All rights reserved.</p>
+    <div class="container-fluid text-center">
+      <ul class="-mx-3 mb-4 lg:hidden">
+        <li class="inline-block mx-3">
+          <a href="" target="_blank" rel="noopener noreferrer" class="inline-block p-3 -m-3">
+            <span class="icon icon-linkedin-dark w-8 h-8"></span>
+          </a>
+        </li>
+        <li class="inline-block mx-3">
+          <a href="" target="_blank" rel="noopener noreferrer" class="inline-block p-3 -m-3">
+            <span class="icon icon-github-dark w-8 h-8"></span>
+          </a>
+        </li>
+      </ul>
+      <p>© 2021 Yu-Chun Wu. All rights reserved.</p>
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-    name: 'AppFooter'
+  name: 'AppFooter'
 }
 </script>
+
+<style lang="postcss" scoped>
+.icon-linkedin-dark {
+  background-image: url('../assets/images/icon-linkedin-dark.svg');
+}
+.icon-github-dark {
+  background-image: url('../assets/images/icon-github-dark.svg');
+}
+</style>
