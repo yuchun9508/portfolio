b4d292ac85752f17d9cedefe463be729a22601d2
Chun <yuchun9508@gmail.com>
2021-10-16 15:33:30 +0800

    try to solve vue-meta prerender issue: update prerender-spa-plugin settings

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,16 @@ module.exports = {
   configureWebpack(config) {
     if (process.env.NODE_ENV === 'production') {
       new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
         routes: ['/'],
         renderer: new Renderer({
+          // Optional - Wait to render until the specified event is dispatched on the document.
+          // eg, with `document.dispatchEvent(new Event('custom-render-trigger'))`
           renderAfterDocumentEvent: 'render-event',
+          // Optional - Wait to render until the specified element is detected using `document.querySelector`
+          renderAfterElementExists: '[data-vue-meta]',
         }),
       })
     }
