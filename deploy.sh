d96ffa3fada9d231c177dea1894471dd70be2567
Chun <yuchun9508@gmail.com>
2021-10-16 12:16:55 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yuchun9508/v2.git main:gh-pages
+git push -f git@github.com:yuchun9508/v2.git master:gh-pages
 
 cd -
