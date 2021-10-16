4fc2cf12bc9ee76ee70e8d6b8743012e9e1f4e2e
Chun <yuchun9508@gmail.com>
2021-10-16 19:29:34 +0800

    try to solve vue-meta prerender issue: update prerender-spa-plugin settings

--- vue.config.js
+++ vue.config.js
@@ -10,14 +10,25 @@ module.exports = {
         new PrerenderSPAPlugin({
           // Required - The path to the webpack-outputted app to prerender.
           staticDir: path.join(__dirname, 'dist'),
+
+          // Optional - The path your rendered app should be output to.
+          // (Defaults to staticDir.)
+          outputDir: path.join(__dirname, 'dist/v2'),
+
+          // Optional - The location of index.html
+          indexPath: path.join(__dirname, 'dist', '/v2/index.html'),
+
           // Required - Routes to render.
           routes: ['/'],
+
           renderer: new Renderer({
             // Optional - Wait to render until the specified event is dispatched on the document.
             // eg, with `document.dispatchEvent(new Event('custom-render-trigger'))`
             renderAfterDocumentEvent: 'render-event',
+
             // Optional - Wait to render until the specified element is detected using `document.querySelector`
             // renderAfterElementExists: '[data-vue-meta]',
+            
             // Other puppeteer options.
             // (See here: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions)
             headless: false // Display the browser window when rendering. Useful for debugging.
