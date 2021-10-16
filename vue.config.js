cc2f0149552ffe244ac805da7ff963e0fa2b2820
Chun <yuchun9508@gmail.com>
2021-10-16 15:07:57 +0800

    add prerender-spa-plugin, related settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/v2/'
-      : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/v2/' : '/',
+  configureWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: ['/'],
+        renderer: new Renderer({
+          renderAfterDocumentEvent: 'render-event',
+        }),
+      })
+    }
+  },
 }
