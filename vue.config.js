2407c84ed99fe3a853e699f7f5d12e2953411eb0
Chun <yuchun9508@gmail.com>
2021-10-16 18:47:33 +0800

    update prerender-spa-plugin settings

--- vue.config.js
+++ vue.config.js
@@ -6,19 +6,24 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/v2/' : '/',
   configureWebpack(config) {
     if (process.env.NODE_ENV === 'production') {
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: ['/'],
-        renderer: new Renderer({
-          // Optional - Wait to render until the specified event is dispatched on the document.
-          // eg, with `document.dispatchEvent(new Event('custom-render-trigger'))`
-          renderAfterDocumentEvent: 'render-event',
-          // Optional - Wait to render until the specified element is detected using `document.querySelector`
-          renderAfterElementExists: '[data-vue-meta]',
-        }),
-      })
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          // Required - The path to the webpack-outputted app to prerender.
+          staticDir: path.join(__dirname, 'dist'),
+          // Required - Routes to render.
+          routes: ['/'],
+          renderer: new Renderer({
+            // Optional - Wait to render until the specified event is dispatched on the document.
+            // eg, with `document.dispatchEvent(new Event('custom-render-trigger'))`
+            renderAfterDocumentEvent: 'render-event',
+            // Optional - Wait to render until the specified element is detected using `document.querySelector`
+            // renderAfterElementExists: '[data-vue-meta]',
+            // Other puppeteer options.
+            // (See here: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions)
+            headless: false // Display the browser window when rendering. Useful for debugging.
+          }),
+        })
+      )
     }
   },
 }
